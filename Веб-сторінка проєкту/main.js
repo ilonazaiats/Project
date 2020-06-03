e99b04625be3e8f904d854115d4d9dc44a839e80
Ilona Zaiats <61309910+ilonazaiats@users.noreply.github.com>
2020-06-03 14:04:12 +0300

    Delete main.js

--- Веб-сторінка проєкту/main.js
+++
@@ -1,7 +0,0 @@
-const gridColors = ['#BCF238', '#FFDF15', '#E6AC40', '#FC813A', '#F24238', '#934238'];
-
-if ($(document).width() > 800) {
-  $('ul#grid li').css('background-color', function(i) {
-    return gridColors[i];
-  })
-}
